1c2c5620a36540c50c4596e648912e522d3fd59c
HP <HP@DESKTOP-6SUN5DR>
2019-05-22 12:08:00 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fourth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter your First Number");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your Second Number");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a - b);
+            Console.ReadLine();
+
+
+        }
+    }
+}
